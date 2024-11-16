6f84f6a307151c4b5a9d85b601c84a5830d09fd6
Shahin Qoli <90387561+shahin-qoli@users.noreply.github.com>
2024-11-16 22:50:14 +0330

    Fix:Models:Added Duration to log

--- loghub/logs/models.py
+++ loghub/logs/models.py
@@ -1,24 +1,30 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-# from category.models import Category
+from category.models import Category
 
 User = get_user_model()
 
 
 class Log(models.Model):
-    description = models.TextField()
-    # category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name="logs")
+    description = models.TextField(max_length=200)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name="logs")
     log_key = models.SlugField(unique=True)
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField()
     is_public = models.BooleanField()
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="logs")
     deleted_at = models.DateTimeField()
-
+    duration = models.DurationField(null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.user}-{self.category}-{self.start_time}"
-
+    
+    def save(self, *args, **kwargs):
+        if self.end_time and self.start_time:
+            self.duration = self.end_time - self.start_time
+        else:
+            self.duration = None
+        super().save(*args, **kwargs)
 
 
     @property
@@ -35,7 +41,7 @@ class SourceLog(models.Model):
     url = models.URLField()
     is_digital = models.BooleanField()
     text = models.TextField()
-    json = models.JSONField()
+    meta_data = models.JSONField()
 
 
     def __str__(self) -> str:
