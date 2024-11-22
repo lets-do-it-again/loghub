6cb755b2f48bf9577f6d7366dc8796b4cc5343b3
taa <TahaM8000@gmail.com>
2024-11-23 01:04:47 +0330

    fix:permission: write permission class for category and use in createlog

--- loghub/logs/urls.py
+++ loghub/logs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CreateLogView
+from .views import *
 
 urlpatterns = [
     path('', CreateLogView.as_view(), name='create-log'),
+    path('update-end-time/', UpdateEndTimeIfNullView.as_view(), name='log-update-end-time'),
+
 ]
--- loghub/logs/views.py
+++ loghub/logs/views.py
@@ -2,11 +2,38 @@ from rest_framework import generics
 from .models import Log
 from .serializers import LogSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LogSerializer
+from django.utils import timezone
+from category.permissions import IsCategoryUser
 
 class CreateLogView(generics.CreateAPIView):
     queryset = Log.objects.all()
     serializer_class = LogSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated , IsCategoryUser]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+
+class UpdateEndTimeIfNullView(generics.UpdateAPIView):
+    """API endpoint to update the end_time of a log entry."""
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
+
+    def post(self, request, *args, **kwargs):
+        log_key = request.data.get('log_key')
+        try:
+            log = self.queryset.get(log_key=log_key)
+        except Log.DoesNotExist:
+            return Response({"detail": "Log not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        if log.end_time is None:
+            log.end_time = timezone.now()
+            log.save()
+            serializer = self.get_serializer(log)
+            return Response(serializer.data)
+        else:
+            return Response({"detail": "End time is already set."}, status=status.HTTP_400_BAD_REQUEST)
